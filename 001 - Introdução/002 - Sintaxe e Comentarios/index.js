250618070c1d6d917c3dcdd401da525c23316503
alexrocha97 <venturarocha08@gmail.com>
2022-10-19 14:17:53 -0300

    repositorio destinado para estudos em javascript e afins

---
+++ 001 - Introdução/002 - Sintaxe e Comentarios/index.js
@@ -0,0 +1,6 @@
+// A sintaxe deve ser igual o javascript pede
+console.log("Olá mundo");
+// Comentei apenas em uma linha
+/*
+Comentario de multiplas linhas
+*/
---
+++ 002 - Tipos Primitivos/index.js
@@ -0,0 +1,14 @@
+// String
+console.log("Olá mundo") 
+// esse método sempre pede uma string, padrão sempre utilizados aspas duplas
+
+console.log('"Texto com aspas"')
+console.log(`'"'"'`)
+// Saída: "Texto com aspas"
+
+/*
+    Como podemos escrever uma string
+    'String'
+    "String"
+    `String`
+*/
