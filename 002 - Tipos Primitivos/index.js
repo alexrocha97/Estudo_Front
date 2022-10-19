a0e10c5251134d1398d8db46b0e33d9d2a0a7f7e
alexrocha97 <venturarocha08@gmail.com>
2022-10-19 15:43:31 -0300

    Inicio

--- 002 - Tipos Primitivos/index.js
+++ 002 - Tipos Primitivos/index.js
@@ -1,9 +1,10 @@
-// String
+// STRING
 console.log("Olá mundo") 
 // esse método sempre pede uma string, padrão sempre utilizados aspas duplas
 
 console.log('"Texto com aspas"')
 console.log(`'"'"'`)
+console.log(String(123))
 // Saída: "Texto com aspas"
 
 /*
@@ -12,3 +13,84 @@ console.log(`'"'"'`)
     "String"
     `String`
 */
+
+
+// NUMBER
+// Sempre utilize Number mais Number, para não gerar bugs
+console.log(typeof Number("123"));
+console.log(typeof "123");
+console.log(123)
+console.log("123" + 1) // Com mais ele concatena
+console.log(Number("123") + 1)
+console.log("123" - 1) // Com menos ele diminue
+
+
+// BOOLEAN
+// Ele sempre retorna false ou true
+// === é comparação
+console.log(1 === 2)
+console.log(2 === 2)
+
+/*
+    BOOLEAN é verdadeiro ou falso
+*/
+
+
+// UNDEFINED E NULL
+console.log(undefined) // algo que não foi indefinido
+console.log(null) // algo que não existe
+console.log(null === undefined)
+
+/*
+    UNDEFINED = algo que não foi definido no momento
+    NULL = algo que veio nulo
+*/
+
+
+// ARRAY
+// 0 => "Dener", 1 => "Alex"
+// length é usado para contar os valores na lista
+console.log(["Dener", "Alex"].length)
+console.log(["Dener", "Alex"])
+console.log([1,2,3,45,"Olá mundo", true])
+// Iteração
+["Alex", "Ventura"].forEach((res, index) => {
+        console.log(res, index)
+});
+
+// Object
+/*
+    Objeto: Tenis(Propriedade) "cadarço", "palmilha",
+    Objeto: Celular(Propriedade) "Fone de ouvido", "Capinha",
+    Objeto: Drone(Propriedade)
+
+    Propriedade / Atributos / Funcionalidade
+*/
+console.log(
+    [
+        {
+            tenis: ["cadarço", "palmilha"],
+            celular: "Fone de ouvido",
+            drone: {
+                preco: 150,
+                camera: "G56-M",
+                personalizacao: {
+                    cor: "Preto",
+                    origem: "BR"
+                },
+                Isdisponivel: false
+            }
+        },
+        {
+            drone: {
+                preco: 300,
+                camera: "G300-X",
+                personalizacao: {
+                    cor: "Vermelho",
+                    origem: "EUA"
+                },
+                Isdisponivel: true
+            }
+        }
+    ]
+)
---
+++ 003 - Operadores Aritiméticos/index.js
@@ -0,0 +1,53 @@
+// O operador de adição ele soma mas também ele concatena. Tome cuidado
+console.log(1 + 1); // Adição
+console.log(25 + 50)
+console.log("Alex " + "Ventura");
+console.log("Nome: " + "Alex Ventura " + ", " + "Idade: " + (24 + 1));
+
+
+// Subtração
+console.log(1 - 1);
+console.log("1" - 1);
+
+// Multiplicação e Divisão
+console.log(2 * 10);
+console.log(10 * 5);
+
+console.log(50 / 10);
+console.log(20 / 10);
+
+// Remainder
+// Ele sempre traz o resto da divisão
+console.log(4 % 2);
+console.log(4 % 3);
+
+// Atribuição
+/*
+    let, var -- pode ser alterado
+    const - não pode ser alterado
+
+    =, +=, -=, *=, /=
+*/
+var a = 6;
+var b = 2;
+
+a += 1; // Atribuição de mais um
+a -= 1; // Retirando menos um
+a *= 2; // Multiplicando mais dois
+a /= 3; // Dividindo
+
+console.log(a);
+console.log(a + b);
+
+
+// OPERADORES DE COMPARAÇÃO
+/*
+    ==,
+    ===,
+    !=,
+    !==,
+    >,
+    <,
+    >=,
+    <=
+*/
