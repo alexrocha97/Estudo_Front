7361b8f5d2a4cd14ed912026439ecb68903ed61e
alexrocha97 <venturarocha08@gmail.com>
2022-10-21 13:24:24 -0300

    Estudos sobre funções construtoras

--- 007 - Funções/index.js
+++ 007 - Funções/index.js
@@ -51,3 +51,86 @@ const HelloArrow = () => {
 function HelloFunction(){
     return console.log("Hello")
 }
+
+
+// Function Arrow possuem this léxico enquanto o modelo normal possui this dinâmico.
+const lanches = {
+    cardapio: [
+        { nome: "Pão", preço: "R$ 10" },
+        { nome: "Presunto", preço: "R$ 5" },
+    ],
+
+    // A function ela pega o contexto do seu "pai" ou contexto global
+    meuPedidoFunction: function (select) {
+        return console.log(this.cardapio[select]);
+    },
+
+    // O contexto da arrow function é dentro dele, fora não funcionará
+    // o this sempre vai pegar a herança do seu pai numa function
+    meuPedidoFunctionArrow: (select) => {
+        this.cardapio = [
+            { nome: "Pão", preço: "R$ 10" },
+            { nome: "Presunto", preço: "R$ 5" },
+        ]
+
+        return console.log(this.cardapio[select]);
+    },
+
+    // Usando o bind você poderá acessar o contexto de fora caso passe para outra function dentro de outra function
+    meuPedidoFunctionTimeOut: function () {
+        setTimeout(function () {
+            console.log(this.cardapio);
+            console.log(this);
+        }.bind(this), 1000)
+    },
+};
+
+
+// IIFE (Expressão de função invocada imediatamente)
+(function (num1, num2) { 
+    const cal = num1 + num2
+    console.log(cal)
+ })(1,2);
+
+
+// Strict mode elimina alguns erros silenciosos 
+// que passariam batido do JavaScript e os faz emitir erros
+"use strict";
+
+(() => {
+    teste = "Hello"
+})();
+
+console.log(teste)
+
+
+// Funções Construtoras
+function Name(name, sobrenome) {
+    this.name = name;
+    this.sobreNome = () => {
+        const nomeCompleto = `${this.name} ${sobrenome}`
+        return nomeCompleto;
+    };
+}
+const dener = new Name("Alex", "Rocha");
+// const teste2 = new Name("Rocha");
+
+function Calculadora() {
+    this.soma = (num1, num2) => {
+        return `${num1 + num2}`;
+    };
+
+    this.sobretracao = (num1, num2) => {
+        return `${num1 - num2}`;
+    }
+}
+
+const opcaoCal = new Calculadora();
+console.log(opcaoCal.soma(1, 2))
+console.log(opcaoCal.sobretracao(1, 2))
+
+
+// console.log(dener);
+// console.log(dener.sobreNome());
+// console.log(dener.name);
+// console.log(teste2.name);
