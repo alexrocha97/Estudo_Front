e21b44b83fe84d5ff364a835e3fc2e9ed5598191
alexrocha97 <venturarocha08@gmail.com>
2022-10-26 22:45:19 -0300

    Fim dos estudos de base do JavaScript

--- 010 - DOM/006-  Formulários/script.js
+++ 010 - DOM/006-  Formulários/script.js
@@ -1,7 +1,9 @@
 "use strict";
 
 
-const form = document.forms.namedItem("registro");
+const form = document.forms.namedItem("select-checkbox");
+const submit = document.querySelector("#submit");
+const change = document.querySelector("#change");
 
 // form.addEventListener('submit', (event) => {
 //     event.preventDefault();
@@ -21,4 +23,57 @@ const form = document.forms.namedItem("registro");
 //     }
 // });
 
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const name  = form.nome.value;
 
+//     const newDivElement = document.createElement("div");
+//     newDivElement.innerText = name;
+//     form.nextElementSibling.remove();
+//     form.after(newDivElement);
+// });
+
+
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+    
+//     const radio = form.radio;
+//     submit.innerHTML = radio.value;
+
+
+
+//     // radio.forEach(item => {
+//     //     if(item.checked){
+//     //         console.log(item);
+//     //     }
+//     // });
+//     // console.log(radio.value);
+// });
+
+// form.radio.forEach((item) => {
+//     item.addEventListener("change", (event) => {
+//         const { target } = event
+//         if (target.checked){
+//             // console.log(target.value);
+//             change.innerHTML = target.value;
+//         }
+//     });
+// });
+
+let checkedValue = [];
+
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const checkbox = form.checkbox;
+//     checkedValue = [];
+
+//     checkbox.forEach(element => {
+//         if(element.checked){
+
+//             checkedValue.push(element.value);
+//             console.log(checkedValue);
+//         }
+//     });
+
+//     submit.innerHTML = checkedValue;
+// });
